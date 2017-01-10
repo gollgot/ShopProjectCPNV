3698d328c682cbad60b98602b6d4e5453a069840
Loïc Dessaules <loic.dessaules@cpnv.ch>
2017-01-10 09:14:08 +0100

    Added some basic TDD tests

---
+++ tests/BasicTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testClickNextForBeta()
+    {
+        $this->visit('/')
+             ->click('Shop')
+             ->seePageIs('/shop')
+             ->see('Articles');
+    }
+}
